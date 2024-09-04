17f8adf5f941acefe4bb23d6802872ff0997cbf1
trisna.na <trisna.na@gmail.com>
2024-09-04 22:13:34 +0700

    add img

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -8,7 +8,13 @@ const String EVENT_DATE = 'Minggu, 22 September 2024\nJam 10.00 - 15.00 wib';
 
 const String img_welcome = 'assets/images/reuni_logo.jpg';
 
-const String img_gallery1 = 'assets/images/reuni_banner.jpg';
+const String img_gallery1 = 'https://raw.githubusercontent.com/Karunanidi/wedding_flutter/main/assets/images/reuni_banner.jpg';
+
+const String img_gallery2 = 'https://raw.githubusercontent.com/Karunanidi/wedding_flutter/main/assets/images/reuni_banner.jpg';
+
+const String img_gallery3 = 'https://raw.githubusercontent.com/Karunanidi/wedding_flutter/main/assets/images/reuni_banner.jpg';
+
+const String img_gallery4 = 'https://raw.githubusercontent.com/Karunanidi/wedding_flutter/main/assets/images/reuni_banner.jpg';
 
 const String img_flower = 'assets/images/flower.png';
 
